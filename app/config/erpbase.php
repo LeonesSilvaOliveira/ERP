accf81d548067db62d0b52f290c0cb9ed6cbf4ef
builder <builder@madbuilder.com.br>
2024-11-28 19:03:27 +0000

    update2

---
+++ app/config/erpbase.php
@@ -0,0 +1,10 @@
+<?php 
+return[
+    'host' => "127.0.0.1",
+    'name' => "erpbase",
+    'user' => "root",
+    'pass' => "",
+    'type' => "mysql",
+    'prep' => "1",
+    'slog' => "SystemSqlLogService"
+];
