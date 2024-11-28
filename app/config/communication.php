a3959e7cf55e72db5cf933e9afe6c552a9a0fb64
Leones Silva de Oliveira <50273318+LeonesSilvaOliveira@users.noreply.github.com>
2024-11-28 16:40:15 -0300

    changed database

--- app/config/communication.php
+++ app/config/communication.php
@@ -1,10 +1,11 @@
-<?php 
-return[
-    'host' => "127.0.0.1",
-    'name' => "erpbase_communication",
-    'user' => "root",
-    'pass' => "",
-    'type' => "mysql",
-    'prep' => "1",
-    'slog' => "SystemSqlLogService"
+<?php
+return [
+    'host' => "127.0.0.1", 
+    'user' => "leoteste2", 
+    'pass' => "1234", 
+    'type' => "mysql", 
+    'port' => "3306", 
+    'name' => "erpbase2_communication", 
+    'prep' => "1", 
+    'slog' => "SystemSqlLogService", 
 ];
--- app/config/erpbase.php
+++ app/config/erpbase.php
@@ -1,10 +1,11 @@
-<?php 
-return[
-    'host' => "127.0.0.1",
-    'name' => "erpbase",
-    'user' => "root",
-    'pass' => "",
-    'type' => "mysql",
-    'prep' => "1",
-    'slog' => "SystemSqlLogService"
+<?php
+return [
+    'host' => "127.0.0.1", 
+    'user' => "leoteste2", 
+    'pass' => "1234", 
+    'type' => "mysql", 
+    'port' => "3306", 
+    'name' => "erpbase2", 
+    'prep' => "1", 
+    'slog' => "SystemSqlLogService", 
 ];
