d85f6dd342212c1796a9d1e1e4687bb0f58b27cf
builder <builder@madbuilder.com.br>
2024-11-28 17:45:41 +0000

    update2

--- app/control/ponto/pontoEletronico.php
+++ app/control/ponto/pontoEletronico.php
@@ -36,7 +36,7 @@ class pontoEletronico extends TPage
 
         $horarioAtualHidden = new THidden('horarioAtualHidden');
         $horaRelogio = new TButton('horaRelogio');
-        $button_1_entrar = new TButton('button_1_entrar');
+        $button_entrar = new TButton('button_entrar');
         $button_2_pausa = new TButton('button_2_pausa');
         $button_3_retorno = new TButton('button_3_retorno');
         $button_4_saida = new TButton('button_4_saida');
@@ -44,28 +44,28 @@ class pontoEletronico extends TPage
 
         $horarioAtualHidden->setSize(200);
         $horaRelogio->setAction(new TAction([$this, 'onButton']), "");
+        $button_entrar->setAction(new TAction([$this, 'onEntrada']), "Entrar");
         $button_2_pausa->setAction(new TAction([$this, 'onPausa']), "2 - Pausa");
         $button_4_saida->setAction(new TAction([$this, 'onSair']), "4 - Saída");
-        $button_1_entrar->setAction(new TAction([$this, 'onEntrada']), "1 - Entrar");
         $button_3_retorno->setAction(new TAction([$this, 'onRetorno']), "3 - Retorno");
 
         $horaRelogio->addStyleClass('horario');
+        $button_entrar->addStyleClass('btn-default');
         $button_2_pausa->addStyleClass('btn-default');
         $button_4_saida->addStyleClass('btn-default');
-        $button_1_entrar->addStyleClass('btn-default');
         $button_3_retorno->addStyleClass('btn-default');
 
         $horaRelogio->setImage(' #000000');
         $button_2_pausa->setImage('fas:coffee #000000');
+        $button_entrar->setImage('fas:door-open #000000');
         $button_3_retorno->setImage('fas:coffee #000000');
-        $button_1_entrar->setImage('fas:door-open #000000');
         $button_4_saida->setImage('fas:door-closed #000000');
 
 
         $row1 = $this->form->addFields([new TLabel("Escolha o tipo de ponto: ", null, '14px', null, '100%')],[$horarioAtualHidden],[$horaRelogio]);
         $row1->layout = [' col-sm-6',' col-sm-6',' col-sm-9'];
 
-        $row2 = $this->form->addFields([$button_1_entrar],[$button_2_pausa]);
+        $row2 = $this->form->addFields([$button_entrar],[$button_2_pausa]);
         $row2->layout = ['col-sm-6','col-sm-6'];
 
         $row3 = $this->form->addFields([$button_3_retorno],[$button_4_saida]);
